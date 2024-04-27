89635169781db9d77b2de0f64ecd6bbd58377b91
minsky922 <minsky922@gmail.com>
2024-04-27 20:57:45 +0900

    blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남우동맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+  //setModal = state를 변경하는 함수
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{ color: 'red', fontSize: '16px' }}>블로그임</h4>
+      </div>
+
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+      {/* <div className="list">
+        <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4 onClick={() => { setModal(!modal) }}>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div> */}
+      {
+        글제목.map(function (글, i) {
+          return (
+            <div className="list" key={i}>
+              <h4 onClick={() => { setModal(!modal); setTitle(i) }}>
+                {글제목[i]} <span onClick={() => {
+                  let copy = [...따봉];
+                  copy[i]++;
+                  따봉변경(copy);
+                }}>👍</span>
+                {따봉[i]}
+              </h4>
+              <button onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+              }}>삭제</button>
+            </div>)
+        })
+      }
+
+      {/* <button onClick={() => { setTitle(0) }}>글제목0</button>
+      <button onClick={() => { setTitle(1) }}>글제목1</button>
+      <button onClick={() => { setTitle(2) }}>글제목2</button> */}
+
+      <input onChange={(e) => {
+        입력값변경(e.target.value);
+      }} />
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy.unshift(입력값);
+        글제목변경(copy)
+      }}>글발행</button>
+
+      {
+        modal === true
+          ? <Modal
+            title={title}
+            color={'skyblue'}
+            글제목={글제목}
+            글제목변경={글제목변경}></Modal>
+          : null
+      }
     </div>
   );
+}
+
+
+function Modal(props) {
+  return (
+    <div className='modal' style={{ background: props.color }}>
+      <h4>{props.글제목[props.title]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={() => {
+        let copy = [...props.글제목];
+        copy[0] = '여자 코트 추천';
+        props.글제목변경(copy);
+      }}>글수정</button>
+    </div >
+  )
 }
 
 export default App;
